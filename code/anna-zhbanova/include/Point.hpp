d31c1e8f49bec76448f82a699d24b021c8c26e02
ZhbanovaAnna <annazhb@gmail.com>
2013-10-04 16:03:50 +0400

    Разработала интерфейс класса

---
+++ code/anna-zhbanova/include/Point.hpp
@@ -0,0 +1,11 @@
+﻿struct PointXY 
+{
+	int x;
+	int y; 
+
+	PointXY (int x_=0, int y_=0)
+	{
+		x = x_;
+		y = y_;
+	}
+};
---
+++ code/anna-zhbanova/include/Triangle.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "PointXY.hpp"
+
+class Triangle
+{
+public:
+	Triangle();
+	Triangle(PointXY a_, PointXY b_, PointXY c_);
+	virtual ~Triangle(void);
+	
+	float AB();
+	float BC();
+	float AC();
+
+	float AngleA();
+	float AngleB();
+	float AngleC();
+
+	float Square();
+	float Perimeter();
+
+	bool IsCorrect();
+	bool IsEquilateral();
+	bool IsIsosceles();
+
+	PointXY Get_a();
+	PointXY Get_b();
+	PointXY Get_c();
+
+	void Set_a(PointXY a_);
+	void Set_b(PointXY b_);
+	void Set_c(PointXY c_);
+private:
+	PointXY a;
+	PointXY b;
+	PointXY c;
+};
