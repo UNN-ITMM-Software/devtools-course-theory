f37c50e3eb5aca5b5a794229d05cf0a90cc76df7
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2013-10-17 11:10:43 +0400

    fixed a bug, improved the accuracy of calculations

--- code/dmitry-vodopyanov/src/LengthConvertor.cpp
+++ code/dmitry-vodopyanov/src/LengthConvertor.cpp
@@ -6,16 +6,11 @@ LengthConvertor::~LengthConvertor(void) {}
 
 Length LengthConvertor::Convert(Length length, LengthUnit UnitOutput)
 {
-	double multiplierToMeter[7] = {0.0254, 0.305, 0.914, 1609.344, 1, 1000, 0.01};
-	double multiplierFromMeter[7] = {39.37, 3.281, 1.094, 0.000621, 1, 0.001, 100};
-	if (length.value > 0)
-	{
+	double multiplierToMeter[7] = {0.0253999998, 0.3048, 0.914399999, 1609.34401, 1, 1000, 0.01};
+	double multiplierFromMeter[7] = {39.370079, 3.2808399, 1.0936133, 0.00062137119, 1, 0.001, 100};
+	if (length.value >= 0)
 		length.value = multiplierToMeter[length.UnitInput]*multiplierFromMeter[UnitOutput]*length.value;
-		return length;
-	}
 	else
-	{
-		length.value = -1;
-		return length;
-	}	
+		length.value = -1;	
+	return length;
 }
