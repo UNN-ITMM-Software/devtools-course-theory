bda88d0518bdff0f00e0193f9a3b176e0a810ea1
juraam <juramoshkov@gmail.com>
2013-10-18 20:26:06 +0400

    change sample

--- code/moshkov-yuriy/samples/dist.cpp
+++ code/moshkov-yuriy/samples/dist.cpp
@@ -6,7 +6,11 @@ int main(int argc, char* argv[])
 	const int sizeVector = 2;
 	float vector1[sizeVector] = {1, 0};
 	float vector2[sizeVector] = {2, 1};
-	float distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2, L3, sizeVector);
-	printf("%f distance \n", distance);
+	float distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2, L1, sizeVector);
+	printf("%f distance between source vectors in L1 \n", distance);
+	distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2, L4, sizeVector);
+	printf("%f distance between source vectors in L4 \n", distance);
+	distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2, LInfinity, sizeVector);
+	printf("%f distance between source vectors in LInfinity \n", distance);	
 	return 0;
 }
