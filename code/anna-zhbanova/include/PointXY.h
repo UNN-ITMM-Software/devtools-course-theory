6cabb07c6ad4b2081769f07704c04b8a41b36a80
ZhbanovaAnna <annazhb@gmail.com>
2013-10-20 18:50:29 +0400

    Google C++ Style Guide

---
+++ code/anna-zhbanova/include/PointXY.h
@@ -0,0 +1,16 @@
+﻿/* Copyright 2013 Anna Zhbanova */
+#ifndef CODE_ANNA_ZHBANOVA_INCLUDE_POINTXY_H_
+#define CODE_ANNA_ZHBANOVA_INCLUDE_POINTXY_H_
+
+struct PointXY 
+{
+    float x;
+    float y; 
+    
+    PointXY (float x_=0, float y_=0) {
+        x = x_;
+        y = y_;
+    }
+};
+
+#endif  // CODE_ANNA_ZHBANOVA_INCLUDE_POINTXY_H_
