86c4a3b8f14594ff8d859238ef174e8603e5f82b
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2013-10-20 17:24:47 +0400

    empty lines added

--- code/dmitry-vodopyanov/include/LengthConvertor.h
+++ code/dmitry-vodopyanov/include/LengthConvertor.h
@@ -1,16 +1,21 @@
 /* Copyright 2013 Dmitry Vodopyanov */
 #ifndef CODE_DMITRY_VODOPYANOV_INCLUDE_LENGTHCONVERTOR_H_
 #define CODE_DMITRY_VODOPYANOV_INCLUDE_LENGTHCONVERTOR_H_
+
 enum LengthUnit {Inch, Foot, Yard, Mile, Meter, KMeter, CMeter};
+
 struct Length {
     double value;
     LengthUnit UnitInput;
 };
+
 class LengthConvertor {
 public
 :
     LengthConvertor(void);
     virtual ~LengthConvertor(void);
+
     Length Convert(Length length, LengthUnit UnitOutput);
 };
+
 #endif  // CODE_DMITRY_VODOPYANOV_INCLUDE_LENGTHCONVERTOR_H_
