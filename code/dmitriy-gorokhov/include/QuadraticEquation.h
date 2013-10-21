c34b6806688617041ba6b4447bc1532d643f1e36
Dmitriy Gorokhov <gorokhovdmit@gmail.com>
2013-10-21 15:34:06 +0400

    code edited in accordance with Google C++ Style Guide

--- code/dmitriy-gorokhov/include/QuadraticEquation.h
+++ code/dmitriy-gorokhov/include/QuadraticEquation.h
@@ -1,16 +1,21 @@
-#pragma once
+/* Copyright 2013 Dmitriy Gorokhov */
+
+#ifndef CODE_DMITRIY_GOROKHOV_INCLUDE_QUADRATICEQUATION_H_
+#define CODE_DMITRIY_GOROKHOV_INCLUDE_QUADRATICEQUATION_H_
 
 enum RootCount {TWOROOTS, ONEROOT, NOREALROOT, ILLEGALEQUATIONFORM};
 
-class QuadraticEquation
-{
-public:
-	QuadraticEquation();
-	QuadraticEquation(double _a, double _b, double _c);
-	virtual ~QuadraticEquation();
-	void SetCoefficients(double _a, double _b, double _c);
-	int Solve(double &root1, double &root2);
+class QuadraticEquation {
+ public:
+    QuadraticEquation();
+    QuadraticEquation(double _a, double _b, double _c);
+    virtual ~QuadraticEquation();
+    void SetCoefficients(double _a, double _b, double _c);
+    int Solve(double &root1, double &root2);
 
-private:
-	double a, b, c;
+ private:
+    double a, b, c;
 };
+
+#endif // CODE_DMITRIY_GOROKHOV_INCLUDE_QUADRATICEQUATION_H_
+
--- code/dmitriy-gorokhov/src/QuadraticEquation.cpp
+++ code/dmitriy-gorokhov/src/QuadraticEquation.cpp
@@ -1,44 +1,39 @@
-#include "QuadraticEquation.h"
+/* Copyright 2013 Dmitriy Gorokhov */
 #include <math.h>
+#include <QuadraticEquation.h>
 
-QuadraticEquation::QuadraticEquation()
-{
-	a = b = c = 0;
+QuadraticEquation::QuadraticEquation() {
+    a = b = c = 0;
 }
 
-QuadraticEquation::QuadraticEquation(double _a, double _b, double _c)
-{
-	a = _a;
-	b = _b;
-	c = _c;
+QuadraticEquation::QuadraticEquation(double _a, double _b, double _c) {
+    a = _a;
+    b = _b;
+    c = _c;
 }
 
-QuadraticEquation::~QuadraticEquation()
-{
+QuadraticEquation::~QuadraticEquation() {}
+
+void QuadraticEquation::SetCoefficients(double _a, double _b, double _c) {
+    a = _a;
+    b = _b;
+    c = _c;
 }
 
-void QuadraticEquation::SetCoefficients(double _a, double _b, double _c)
-{
-	a = _a;
-	b = _b;
-	c = _c;
+int QuadraticEquation::Solve(double &root1, double &root2) {
+    if(a == 0)
+        return(ILLEGALEQUATIONFORM);
+    double discriminant = b*b - 4*a*c;
+    if(discriminant < 0)
+        return(NOREALROOT);
+    else {
+        root1 = (-b + sqrt(discriminant))/(2*a);
+        root2 = (-b - sqrt(discriminant))/(2*a);
+    }
+    if(discriminant == 0)
+        return(ONEROOT);
+    else
+        return(TWOROOTS);
 }
 
-int QuadraticEquation::Solve(double &root1, double &root2)
-{
-	if(a == 0)
-		return(ILLEGALEQUATIONFORM);
-	double discriminant = b*b - 4*a*c;
-	if(discriminant < 0)
-		return(NOREALROOT);
-	else
-	{
-		root1 = (-b + sqrt(discriminant))/(2*a);
-		root2 = (-b - sqrt(discriminant))/(2*a);
-	}
-	if(discriminant == 0)
-		return(ONEROOT);
-	else 
-		return(TWOROOTS);
-}
 
