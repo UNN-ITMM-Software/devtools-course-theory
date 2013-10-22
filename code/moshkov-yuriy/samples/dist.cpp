7cfa3fb898681c02eddb409435857bda74d51d4e
juraam <juramoshkov@gmail.com>
2013-10-22 20:34:13 +0400

    change cpp, header, sample

--- code/moshkov-yuriy/samples/dist.cpp
+++ code/moshkov-yuriy/samples/dist.cpp
@@ -1,16 +1,19 @@
+/* Copyright 2013 Moshkov Yuriy */
 #include "DistanceBetweenVectors.h"
 #include <stdio.h>
-
-int main(int argc, char* argv[])
-{
-	const int sizeVector = 2;
-	float vector1[sizeVector] = {1, 0};
-	float vector2[sizeVector] = {2, 1};
-	float distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2, L1, sizeVector);
-	printf("%f distance between source vectors in L1 \n", distance);
-	distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2, L4, sizeVector);
-	printf("%f distance between source vectors in L4 \n", distance);
-	distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2, LInfinity, sizeVector);
-	printf("%f distance between source vectors in LInfinity \n", distance);
-	return 0;
+int main(int argc, char* argv[]) {
+    const int sizeVector = 2;
+    float vector1[sizeVector] = {1, 0};
+    float vector2[sizeVector] = {2, 1};
+	float distance = 0;
+    distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2,
+                                                         L1, sizeVector);
+    printf("%f distance between source vectors in L1 \n", distance);
+    distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2,
+                                                         L4, sizeVector);
+    printf("%f distance between source vectors in L4 \n", distance);
+    distance = DistanceBetweenVectors::CalculateDistance(vector1, vector2,
+	                                                    LInfinity, sizeVector);
+    printf("%f distance between source vectors in LInfinity \n", distance);
+    return 0;
 }
--- code/moshkov-yuriy/src/DistanceBetweenVectors.cpp
+++ code/moshkov-yuriy/src/DistanceBetweenVectors.cpp
@@ -1,25 +1,21 @@
+/* Copyright 2013 Moshkov Yuriy */
 #include "DistanceBetweenVectors.h"
 
 
 float DistanceBetweenVectors::CalculateDistance(float *vector1, float *vector2, TypeMetric typeMetric, int sizeVector)
 {
-	float distance;
-	if (typeMetric == LInfinity)
-	{
-		float max = 0;
-		for (int i = 0; i < sizeVector; i++)
-			if (max < fabs(vector1[i] - vector2[i]))
-				max = fabs(vector1[i] - vector2[i]);
-		distance = max;
-	}
-	else
-	{
-		float sumComp = 0;
-		for (int i = 0; i < sizeVector; i++)
-		{
-			sumComp += powf(fabs(vector1[i] - vector2[i]), 1.0f * typeMetric);
-		}
-		distance = powf(sumComp, 1.0f / typeMetric);
-	}
-	return distance;
+    float distance;
+    if (typeMetric == LInfinity) {
+        float max = 0;
+        for (int i = 0; i < sizeVector; i++)
+            if (max < fabs(vector1[i] - vector2[i]))
+                max = fabs(vector1[i] - vector2[i]);
+        distance = max;
+    } else {
+        float sumComp = 0;
+        for (int i = 0; i < sizeVector; i++)
+            sumComp += powf(fabs(vector1[i] - vector2[i]), 1.0f * typeMetric);
+        distance = powf(sumComp, 1.0f / typeMetric);
+    }
+    return distance;
 }
