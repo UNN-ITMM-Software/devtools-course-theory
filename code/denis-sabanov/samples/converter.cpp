ec19d319649b56f49b3a6385c5d146c492ae5206
Denis_Sabanov <denissabanov@mail.ru>
2013-10-25 12:46:05 -0700

    Update converter.cpp

--- code/denis-sabanov/samples/converter.cpp
+++ code/denis-sabanov/samples/converter.cpp
@@ -1,8 +1,10 @@
-#include "AreaConverter.h"
-#include "stdio.h"
+/* Copyright 2013 Denis Sabanov */
+
+#include <AreaConverter.h>
+#include <stdio.h>
 
 int main(){
-	AreaConverter Converter(100, sKilometer);
-	printf("100 kilomerers = %lf foots \n",Converter.ConvertToNewType(sFoot) );
-	return 0;
+    AreaConverter Converter(100, sKilometer);
+    printf("100 kilomerers = %lf foots \n", Converter.ConvertToNewType(sFoot) );
+    return 0;
 }
