333708c6897bea7d8b2f438752f82b0626343e67
A-Kostrov <alexkostrov2012@gmail.com>
2013-10-28 22:28:36 +0400

    Additional style changes#3

--- code/aleksey-kostrov/samples/samples.cpp
+++ code/aleksey-kostrov/samples/samples.cpp
@@ -9,20 +9,22 @@ int main(void) {
     const int TOO_SMALL_ARRAY = -2;
     const int ELEM_NOT_FOUND = -1;
     int * array;
-    int size = 30, element = 9;
+    int size = 20, element = 18;
     printf("Size of array = %d\n", size);
     printf("Search element = %d\n", element);
     srand(time(NULL));
     array = new int[size];
+    unsigned int seed = 10;
     for (int i = 0; i < size; i++)
-        array[i] = rand() % RIGHT_BORDER_RANDOM + LEFT_BORDER_RANDOM;
+        array[i] = rand_r(&seed) % RIGHT_BORDER_RANDOM + LEFT_BORDER_RANDOM;
     if (size < SIZE_FOR_PRINT && size > 0) {
         printf("Generated array: ");
         for (int i = 0; i < size; i++)
             printf("%d ", array[i]);
         printf("\n");
-    } else if (size > 0)
+    } else if (size > 0) {
         printf("Array was generated automatically!\n");
+    }
     try {
         DichotomySearch example1;
         example1.Search(array, element, size);
