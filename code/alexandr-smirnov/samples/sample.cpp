6563b653b783d9b1c0a8d652e30f27d06740af1b
Alex <admin@apelsin-pc.ru>
2013-10-31 22:52:54 +0400

    Added CMakeLists.txt

--- code/alexandr-smirnov/samples/sample.cpp
+++ code/alexandr-smirnov/samples/sample.cpp
@@ -3,6 +3,7 @@
 #include <sort.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 
 int main() {
     printf("This is the program of sorting arrays\n");
@@ -53,4 +54,5 @@ int main() {
     delete a, b, c;
 
     return 0;
+    getch();
 }
