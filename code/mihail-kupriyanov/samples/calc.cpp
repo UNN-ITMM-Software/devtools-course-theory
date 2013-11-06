07e1bffed7b77d0104a0be7815e12b63760d4bcd
mihacooper <mihakuper@gmail.com>
2013-11-06 23:26:19 +0400

    cmake and ctest

--- code/mihail-kupriyanov/samples/calc.cpp
+++ code/mihail-kupriyanov/samples/calc.cpp
@@ -1,19 +1,28 @@
 // Copyright 2013 Michail Kupriyanov
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <vector>
 #include <string>
 
 #include "../include/mathquancalc.h"
 // #include "mathquancalc.h"
 
+void HelpExeption(char* appName) {
+    printf("This is a math quantities calculator application.\n\n");
+    printf("Please provide arguments in the following format:\n\n");
+    printf("  $ %s <value_1> <probability_1> <value_2> <probability2_>"
+        " ... \n\n", appName);
+    printf("Where both arguments are double numbers.\n");
+}
+
 void PrintSample(std::vector<MathQuanCalc::Event> sample) {
-    printf("\n%s", "значения:    ");
-    for (int i = 0; i < sample.size(); i++) {
+    printf("\n%s", "Values:    ");
+    for (unsigned int i = 0; i < sample.size(); i++) {
         printf("%5.2f ", sample[i].value);
     }
-    printf("\n%s", "вероятности: ");
-    for (int i = 0; i < sample.size(); i++) {
+    printf("\n%s", "Probabilitys: ");
+    for (unsigned int i = 0; i < sample.size(); i++) {
         printf("%5.2f ", sample[i].probability);
     }
 }
@@ -25,7 +34,7 @@ void MakeCalc(MathQuanCalc* mqCalc,
         double expVal = 0.;
         try {
             expVal = mqCalc->GetExpectedValue();
-            printf("\n%s %5.2f", "Мат. ожидание:", expVal);
+            printf("\n%s %5.2f", "Expected value:", expVal);
         }
         catch(const std::string& str) {
             printf("\n%s\n", str.c_str());
@@ -33,7 +42,7 @@ void MakeCalc(MathQuanCalc* mqCalc,
         double dispersion = 0.;
         try {
             dispersion = mqCalc->GetDispersion();
-            printf("\n%s %5.2f\n", "Дисперсия:", dispersion);
+            printf("\n%s %5.2f\n", "Dispersion:", dispersion);
         }
         catch(const std::string& str) {
             printf("\n%s\n", str.c_str());
@@ -44,33 +53,47 @@ void MakeCalc(MathQuanCalc* mqCalc,
     }
 }
 
-int main() {
+double ParseDouble(const char* arg) {
+    char* end;
+    double value = strtof(arg, &end);
+
+    if (!end[0]) {
+        printf("%s is valid\n", arg);
+    } else {
+        printf("%s is invalid\n", arg);
+        throw std::string("ERROR: wrong number format");
+    }
+
+    return value;
+}
+
+int main(int argc, char** argv) {
     MathQuanCalc mqCalc;
-    MathQuanCalc::Event    event;
-    std::vector<MathQuanCalc::Event>    sample;
+    MathQuanCalc::Event event;
+    std::vector<MathQuanCalc::Event> sample;
 
-    // создаем и считаем значения для ряда
-    // распределения значения выпадающего на игральном кубике
-    event.probability = 1./6.;
-    for (int i = 1; i <= 6; i++) {
-        event.value = i;
+    if (argc == 1) {
+        HelpExeption(argv[0]);
+        exit(0);
+    }
+    if ((argc % 2) == 0) {
+        printf("ERROR: wrong arguments format.\n\n");
+        HelpExeption(argv[0]);
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i += 2) {
+        try {
+            event.value = ParseDouble(argv[i]);
+            event.probability = ParseDouble(argv[i + 1]);
+        }
+        catch(const std::string& str) {
+            printf("\n%s\n", str.c_str());
+            exit(0);
+        }
         sample.push_back(event);
     }
-    printf("\n%s",
-    "ряд распределения значения выпадающего на игральном кубике: ");
 
-    PrintSample(sample);
-    MakeCalc(&mqCalc, sample);
-    sample.clear();
-
-    // создаем и считаем значения для некоторого
-    // некорректного ряда распределения
-    event.probability = 1./2.;
-    for (int i = 1; i <= 3; i++) {
-        event.value = i;
-        sample.push_back(event);
-    }
-    printf("\n%s", "Некоторый некорректный ряд распределения: ");
     PrintSample(sample);
     MakeCalc(&mqCalc, sample);
     return 0;
