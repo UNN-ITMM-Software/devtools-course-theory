3d9eed471915b6c3da21c1f6e8bc2d08a4dd34f1
AlekseyNesmelov <70508010alex@mail.ru>
2013-11-08 00:07:17 +0300

    Google style

--- code/aleksey-nesmelov/samples/Fraction.cpp
+++ code/aleksey-nesmelov/samples/Fraction.cpp
@@ -23,8 +23,8 @@ Expression parseArguments(int argc, char** argv);
 void help(const char* appname) {
     printf("This is a fraction calculator application.\n\n");
     printf("Please provide arguments in the following format:\n\n");
-    printf("  $ %s <argument1> <argument2> <argument3> <argument4> <operation>\n\n",
-    appname);
+    printf("  $ %s <argument1> <argument2> <argument3>", appname);
+	printf(" <argument4> <operation>\n\n",);
     printf("Where all arguments are integer numbers, ");
     printf("and <operation> is one of '+' , '-' , '*' or '/'.\n");
 }
@@ -62,11 +62,10 @@ Expression parseArguments(int argc, char** argv) {
         printf("Wrong number format!\n");
         exit(2);
     }
-	if (expression.arg2 == 0) {
+    if (expression.arg2 == 0) {
          printf("%s Denominator cannot be zero\n", argv[2]);
     if (expression.arg4 == 0) {
-         printf("%s Denominator cannot be zero\n", argv[4])
-
+         printf("%s Denominator cannot be zero\n", argv[4]);
     }
     expression.operation = *argv[5];
     if ((strlen(argv[5]) != 1) ||
@@ -83,11 +82,11 @@ Expression parseArguments(int argc, char** argv) {
 int main(int argc, char** argv) {
     Expression expr = parseArguments(argc, argv);
     Fraction fract1(expr.arg1, expr.arg2);
-	Fraction fract2(expr.arg3, expr.arg4);
-	Fraction result(0, 1);
+    Fraction fract2(expr.arg3, expr.arg4);
+    Fraction result(0, 1);
 
     switch (expr.operation) {
-	 case '+': 
+     case '+':
         result = Fraction::Add(fract1, fract2);
         printf("Result = %d / %d\n", result.GetNumenator(),
         result.GetDenominator());
