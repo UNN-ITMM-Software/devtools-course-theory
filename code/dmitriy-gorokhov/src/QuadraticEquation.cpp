f236f09e9c9795763a21719c1c987feb8ac9305a
Dmitriy Gorokhov <gorokhovdmit@gmail.com>
2013-11-07 20:32:31 +0400

    Added CTestTests.txt

--- code/dmitriy-gorokhov/src/QuadraticEquation.cpp
+++ code/dmitriy-gorokhov/src/QuadraticEquation.cpp
@@ -5,8 +5,9 @@
 QuadraticEquation::QuadraticEquation(): a(0), b(0), c(0) {
 }
 
-QuadraticEquation::QuadraticEquation(double _a, double _b, double _c):
-    a(_a), b(_b), c(_c) {
+QuadraticEquation::QuadraticEquation(double _a,
+                                     double _b,
+                                     double _c): a(_a), b(_b), c(_c) {
 }
 
 QuadraticEquation::~QuadraticEquation() {}
