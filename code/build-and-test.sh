f2894b77864396dee99609607e3c45448fa27c11
Kirill Kornyakov <kirill.kornyakov@gmail.com>
2013-11-10 17:30:14 +0400

    Enabling CTest output on failure

--- code/build-and-test.sh
+++ code/build-and-test.sh
@@ -69,4 +69,4 @@ cmake_build_dir="../build_cmake"
 $dir=$cmake_build_dir
 mkdir $cmake_build_dir
 cd $cmake_build_dir
-try cmake ../code && make && make test
+try cmake ../code && make && ctest --output-on-failure
