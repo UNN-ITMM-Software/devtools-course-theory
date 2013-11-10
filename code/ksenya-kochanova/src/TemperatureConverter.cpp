01f92b2c4c93244d61e20115defdb177b2b932a4
Ksenya <kape1ka_12@mail.ru>
2013-11-10 21:19:32 +0400

    Исправления.

--- code/ksenya-kochanova/src/TemperatureConverter.cpp
+++ code/ksenya-kochanova/src/TemperatureConverter.cpp
@@ -15,25 +15,8 @@ fromTemperature) {
     double a[4] = {1, 1, 5/9, 100/33};
     double b[4] = {0, -273.15, -32, 0};
     Temperature inCelsius;
-    if ((fromTemperature.unit == 0)&&(fromTemperature.value < -273.15)) {
-         flag = 0;
-         inCelsius.value = 0;
-    }
-    if ((fromTemperature.unit == 1)&&(fromTemperature.value < 0)) {
-         flag = 0;
-         inCelsius.value = 0;
-    }
-    if ((fromTemperature.unit == 2)&&(fromTemperature.value < -459.67)) {
-         flag = 0;
-         inCelsius.value = 0;
-    }
-    if ((fromTemperature.unit == 3)&&(fromTemperature.value < -90.14)) {
-         flag = 0;
-         inCelsius.value = 0;
-    } else {
-        inCelsius.value = a[fromTemperature.unit] * fromTemperature.value
+    inCelsius.value = a[fromTemperature.unit] * fromTemperature.value
 + b[fromTemperature.unit];
-     }
     return inCelsius;
 }
 Temperature TemperatureConvertor:: ConvertFromCelsius(int flag,
@@ -41,12 +24,6 @@ Temperature inCelsius, TemperatureUnit toUnit) {
     double a[4] = {1, 1, 5/9, 100/33};
     double b[4] = {0, -273.15, -32, 0};
     Temperature outTemperature;
-    if (flag == 0) {
-        printf("Erorr");
-        exit(0);
-        outTemperature.value = 12345;
-    } else {
-        outTemperature.value = 1 / a[toUnit] * (inCelsius.value - b[toUnit]);
-    }
+    outTemperature.value = 1 / a[toUnit] * (inCelsius.value - b[toUnit]);
     return outTemperature;
 }
