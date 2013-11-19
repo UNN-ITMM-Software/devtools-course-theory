feeb7329edf579ec7f5e1e7c7769edfbd921e7e6
Kirill Kornyakov <kirill.kornyakov@gmail.com>
2013-11-19 08:58:43 +0400

    Added folder for lab5
    Fixed build

---
+++ code/kirill-kornyakov-lab5/include/simplecalc.h
@@ -0,0 +1,15 @@
+// Copyright 2013 Kirill Kornyakov
+
+#ifndef CODE_KIRILL_KORNYAKOV_LAB5_INCLUDE_SIMPLECALC_H_
+#define CODE_KIRILL_KORNYAKOV_LAB5_INCLUDE_SIMPLECALC_H_
+
+class SimpleCalculator {
+ public:
+    SimpleCalculator();
+    ~SimpleCalculator();
+
+    int Add(int a, int b);
+    int Sub(int a, int b);
+};
+
+#endif  // CODE_KIRILL_KORNYAKOV_LAB5_INCLUDE_SIMPLECALC_H_
