6edc5ac11f3bd06e880cbe0cd3d1d521004afb84
Kirill Kornyakov <kirill.kornyakov@gmail.com>
2013-11-28 22:02:31 +0400

    Removed commented code

--- code/3rdparty/gtest/disable_warnings.h
+++ code/3rdparty/gtest/disable_warnings.h
@@ -1,19 +1,9 @@
-// #if defined(__GNUC__) || defined(__clang__)
-//     #pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wundef"
+#pragma GCC diagnostic ignored "-Wpadded"
+#pragma GCC diagnostic ignored "-Wmissing-noreturn"
 
-    #pragma GCC diagnostic ignored "-Wundef"
-    #pragma GCC diagnostic ignored "-Wpadded"
-    #pragma GCC diagnostic ignored "-Wmissing-noreturn"
-// #endif
-
-// #if defined(__clang__)
-    #pragma clang diagnostic ignored "-Wweak-vtables"
-    #pragma clang diagnostic ignored "-Wglobal-constructors"
-    #pragma clang diagnostic ignored "-Wused-but-marked-unused"
-    #pragma clang diagnostic ignored "-Wshift-sign-overflow"
-    #pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
-// #endif
-
-// #if defined(__GNUC__) || defined(__clang__)
-//     #pragma GCC diagnostic pop
-// #endif
+#pragma clang diagnostic ignored "-Wweak-vtables"
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+#pragma clang diagnostic ignored "-Wused-but-marked-unused"
+#pragma clang diagnostic ignored "-Wshift-sign-overflow"
+#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
