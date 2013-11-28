afce2158450631ef16a6f2525c24bd1bb63927ec
Kirill Kornyakov <kirill.kornyakov@gmail.com>
2013-11-28 22:20:15 +0400

    Fixed issue with MSVS

--- code/3rdparty/gtest/disable_warnings.h
+++ code/3rdparty/gtest/disable_warnings.h
@@ -1,10 +1,14 @@
-#pragma GCC diagnostic ignored "-Weffc++"
-#pragma GCC diagnostic ignored "-Wundef"
-#pragma GCC diagnostic ignored "-Wpadded"
-#pragma GCC diagnostic ignored "-Wmissing-noreturn"
+#if defined(__GNUC__)
+    #pragma GCC diagnostic ignored "-Weffc++"
+    #pragma GCC diagnostic ignored "-Wundef"
+    #pragma GCC diagnostic ignored "-Wpadded"
+    #pragma GCC diagnostic ignored "-Wmissing-noreturn"
+#endif
 
-#pragma clang diagnostic ignored "-Wweak-vtables"
-#pragma clang diagnostic ignored "-Wglobal-constructors"
-#pragma clang diagnostic ignored "-Wused-but-marked-unused"
-#pragma clang diagnostic ignored "-Wshift-sign-overflow"
-#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+#if defined(__clang__)
+    #pragma clang diagnostic ignored "-Wweak-vtables"
+    #pragma clang diagnostic ignored "-Wglobal-constructors"
+    #pragma clang diagnostic ignored "-Wused-but-marked-unused"
+    #pragma clang diagnostic ignored "-Wshift-sign-overflow"
+    #pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+#endif
