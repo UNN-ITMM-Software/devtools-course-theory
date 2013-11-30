88b478334e77698d1a28a4fdc8ed99478f43afa9
juraam <juramoshkov@gmail.com>
2013-11-30 15:48:08 +0400

    add classApplication

--- code/moshkov-yuriy/library/DistanceBetweenVectors.h
+++ code/moshkov-yuriy/library/DistanceBetweenVectors.h
@@ -1,15 +1,14 @@
 /* Copyright 2013 Moshkov Yuriy */
-#ifndef CODE_MOSHKOV_YURIY_INCLUDE_DISTANCEBETWEENVECTORS_H_
-#define CODE_MOSHKOV_YURIY_INCLUDE_DISTANCEBETWEENVECTORS_H_
+#ifndef CODE_MOSHKOV_YURIY_LIBRARY_DISTANCEBETWEENVECTORS_H_
+#define CODE_MOSHKOV_YURIY_LIBRARY_DISTANCEBETWEENVECTORS_H_
 #include <math.h>
 
 enum TypeMetric {L1 = 1, L2, L3, L4, LInfinity};
 
 class DistanceBetweenVectors {
-public
-:
+public:
     static float CalculateDistance(float *vector1, float *vector2,
                                    TypeMetric typeMetric, int sizeVector);
 };
 
-#endif  // CODE_MOSHKOV_YURIY_INCLUDE_DISTANCEBETWEENVECTORS_H_
+#endif  // CODE_MOSHKOV_YURIY_LIBRARY_DISTANCEBETWEENVECTORS_H_
---
+++ code/moshkov-yuriy/library/src/DistanceBetweenVectors.cpp
@@ -0,0 +1,37 @@
+/* Copyright 2013 Moshkov Yuriy */
+#include "library/DistanceBetweenVectors.h"
+#include <string>
+#include <float.h>
+
+void CheckRange(float result) {
+    if (result != 0.0)
+    {
+        if (result > FLT_MAX)
+            throw std::string("The result is larger than FLT_MAX");
+        else if (result < -FLT_MAX)
+            throw std::string("The result is less than -FLT_MAX");
+        else if (result < FLT_MIN && result > 0.0)
+            throw std::string("The result is less than FLT_MIN and larger 0");
+        else if (result > -FLT_MIN && result < 0.0)
+            throw std::string("The result is larger than -FLT_MIN and less 0");
+    }
+}
+
+float DistanceBetweenVectors::CalculateDistance(float *vector1, float *vector2,
+                                       TypeMetric typeMetric, int sizeVector) {
+    float distance;
+    if (typeMetric == LInfinity) {
+        float max = 0;
+        for (int i = 0; i < sizeVector; i++)
+            if (max < fabs(vector1[i] - vector2[i]))
+                max = fabs(vector1[i] - vector2[i]);
+        distance = max;
+    } else {
+        float sumComp = 0;
+        for (int i = 0; i < sizeVector; i++)
+            sumComp += powf(fabs(vector1[i] - vector2[i]), 1.0f * typeMetric);
+        distance = powf(sumComp, 1.0f / typeMetric);
+    }
+    CheckRange(distance);
+    return static_cast<float>(distance);
+}
