b4d7d0c9f0fff4d50172fcb19680a9ab478e45d3
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2013-12-04 00:11:25 +0400

    again and again...

--- code/dmitry-vodopyanov/sample/LengthConvertor.cpp
+++ code/dmitry-vodopyanov/sample/LengthConvertor.cpp
@@ -1,9 +1,8 @@
 /* Copyright 2013 Dmitry Vodopyanov */
 
-
-#include <stdio.h>
-#include "library/LengthConvertorApp.h"
 #include <string>
+#include "library/LengthConvertorApp.h"
+#include <stdio.h>
 
 int main(int argc, char** argv) {
     LengthConvertorApp app;
