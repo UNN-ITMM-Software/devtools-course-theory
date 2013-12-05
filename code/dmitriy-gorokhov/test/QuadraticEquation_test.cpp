cd13c09aa439d88fe54ce64ac3fd1e2efcdee810
Dmitriy Gorokhov <gorokhovdmit@gmail.com>
2013-12-06 01:13:11 +0400

    Changed QuadraticEquation.cpp

--- code/dmitriy-gorokhov/test/QuadraticEquation_test.cpp
+++ code/dmitriy-gorokhov/test/QuadraticEquation_test.cpp
@@ -51,6 +51,6 @@ TEST_F(QuadraticEquationTest, can_Solve_Equation_With_Big_Coefficients) {
 TEST_F(QuadraticEquationTest, can_Solve_Equation_With_Small_Coefficients) {
     quadEq.SetCoefficients(0.000001, 0.0000076, -0.0000193);
     quadEq.Solve(&Root1, &Root2);
-    EXPECT_EQ(2.0086142925830424, Root1);
+    EXPECT_EQ(2.008614292583042, Root1);
     EXPECT_EQ(-9.608614292583043, Root2);
 }
