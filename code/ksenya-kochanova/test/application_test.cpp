a0acd111386620ee9467c8789a5812f10a58b0d6
Ksenya <ksenya.kochanova2gmail.com>
2013-12-07 01:49:57 +0400

    Google stile

--- code/ksenya-kochanova/test/application_test.cpp
+++ code/ksenya-kochanova/test/application_test.cpp
@@ -10,7 +10,6 @@ using ::testing::internal::RE;
 
 class AppTest: public ::testing::Test {
  protected:
-
     void RunApp(int argc, const char* argv[]) {
         output_ = app_(argc, argv);
     }
