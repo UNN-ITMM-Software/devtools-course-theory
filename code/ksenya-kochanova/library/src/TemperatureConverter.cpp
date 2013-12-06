cef64b9e475dbf29b8619f77b54d68fe9db1f959
Ksenya <ksenya.kochanova2gmail.com>
2013-12-06 20:33:47 +0400

    Edit TemperatureConverter.cpp

--- code/ksenya-kochanova/library/src/TemperatureConverter.cpp
+++ code/ksenya-kochanova/library/src/TemperatureConverter.cpp
@@ -5,14 +5,14 @@
 #include <stdint.h>
 #include <limits.h>
 #include <string>
+    double a[4] = {1, 1, 0.5555555555555556, 3.0303030303030303};
+    double b[4] = {0, -273.15, -32, 0};
 
 TemperatureConvertor::TemperatureConvertor() {}
 TemperatureConvertor::~TemperatureConvertor() {}
 
 Temperature TemperatureConvertor::ConvertToCelsius(Temperature
 fromTemperature) {
-    double a[4] = {1, 1, 0.5555555555555556, 3.0303030303030303};
-    double b[4] = {0, -273.15, -32, 0};
     Temperature inCelsius;
     inCelsius.value = a[fromTemperature.unit] * fromTemperature.value
 + b[fromTemperature.unit];
@@ -20,8 +20,6 @@ fromTemperature) {
 }
 Temperature TemperatureConvertor::ConvertFromCelsius(Temperature inCelsius,
 TemperatureUnit toUnit) {
-    double a[4] = {1, 1, 0.5555555555555556, 3.0303030303030303};
-    double b[4] = {0, -273.15, -32, 0};
     Temperature outTemperature;
     outTemperature.value = 1 / a[toUnit] * (inCelsius.value - b[toUnit]);
     return outTemperature;
@@ -29,7 +27,7 @@ TemperatureUnit toUnit) {
 Temperature TemperatureConvertor::Convert(double value,
                            TemperatureUnit fromUnit, TemperatureUnit toUnit) {
     Temperature t;
-        if (((value < -273.15))&&(fromUnit == Celsius)||
+        if ((value < -273.15)&&(fromUnit == Celsius)||
             ((value < 0)&&(fromUnit == Kelvin))||
             ((value < -459.67)&&(fromUnit == Fahrenheit))||
             ((value < -90.14)&&(fromUnit == Newton))||
