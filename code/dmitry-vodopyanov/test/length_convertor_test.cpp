9f4f116b752bc501b424820f271954eda87ad6ba
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2013-12-07 15:48:24 +0400

    fixed implementation of the tests

--- code/dmitry-vodopyanov/test/length_convertor_test.cpp
+++ code/dmitry-vodopyanov/test/length_convertor_test.cpp
@@ -14,18 +14,18 @@ class LengthConvertorTest : public ::testing::Test {
     LengthConvertor convertor;
 };
 
-TEST_F(LengthConvertor, Can_Convert_KMeters_to_Meters) {
-    EXPECT_EQ(1300.000000, convertor.Convert({1.3, km}, m));
+TEST_F(LengthConvertorTest, Can_Convert_KMeters_to_Meters) {
+    EXPECT_EQ(1300.000000, convertor.Convert({1.3, km}, m).value);
 }
 
-TEST_F(LengthConvertor, Do_Throw_Exception_When_Value_Is_Larger_Than_DBL_MAX) {
+TEST_F(LengthConvertorTest, Do_Throw_Exception_When_Value_Is_Larger_Than_DBL_MAX) {
     EXPECT_THROW(convertor.Convert({DBL_MAX, m}, in), std::string);
 }
 
-TEST_F(LengthConvertor, Do_Throw_Excep_When_Value_Is_Less_Than_Minus_DBL_MAX) {
+TEST_F(LengthConvertorTest, Do_Throw_Excep_When_Value_Is_Less_Than_Minus_DBL_MAX) {
     EXPECT_THROW(convertor.Convert({-DBL_MAX, m}, in), std::string);
 }
 
-TEST_F(LengthConvertor, Can_Detect_Negative_Number) {
-    EXPECT_EQ(-1.000000, convertor.Convert({-5, in}, m);
+TEST_F(LengthConvertorTest, Can_Detect_Negative_Number) {
+    EXPECT_EQ(-1.000000, convertor.Convert({-5, in}, m).value);
 }
