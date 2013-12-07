34f2cbcda5c7566fb8033367b83a45b168429d85
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2013-12-07 22:12:53 +0400

    fixed the tests

--- code/dmitry-vodopyanov/test/application_test_refactored.cpp
+++ code/dmitry-vodopyanov/test/application_test_refactored.cpp
@@ -42,7 +42,7 @@ TEST_F(AppTestR, Is_Checking_Number_Of_Arguments) {
 
     RunApp(argc, argv);
 
-    Check("ERROR: Should be 3 arguments\\.");
+    Check("ERROR: Should be 3 arguments\\..");
 }
 
 TEST_F(AppTestR, Can_Detect_Wrong_Unit_Format) {
