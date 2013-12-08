785447e4fd773f1c754d3a0db7905b7f5dcb4462
Denis_Sabanov <denissabanov@mail.ru>
2013-12-08 12:53:19 -0800

    Update converter.cpp

--- code/denis-sabanov/samples/converter.cpp
+++ code/denis-sabanov/samples/converter.cpp
@@ -1,13 +1,5 @@
-// ConsoleApplication3.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
-
 #include "AreaConverterApp.h"
 #include <iostream>
-using namespace std;
-
-
 
 int main(int argc, char** argv) {
 	AreaConverterApp app;
