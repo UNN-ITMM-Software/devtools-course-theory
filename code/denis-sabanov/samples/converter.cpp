f50e066e16737fa52cc4c47eb13bba7f3364b885
Denis_Sabanov <denissabanov@mail.ru>
2013-12-08 13:15:34 -0800

    Update converter.cpp

--- code/denis-sabanov/samples/converter.cpp
+++ code/denis-sabanov/samples/converter.cpp
@@ -1,12 +1,12 @@
-#include "AreaConverterApp.h"
-#include <iostream>
+/* Copyright 2013 Denis Sabanov */
+
+#include "library/AreaConverterApp.h"
+#include <string>
 
 int main(int argc, char** argv) {
-	AreaConverterApp app;
+    AreaConverterApp app;
     std::string output = app(argc, argv);
     printf("%s\n", output.c_str());
 
     return 0;
 }
-	
-
