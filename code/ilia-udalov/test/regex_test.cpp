702ccebb23938e7613e2dd34a137de3945d7a4d3
Ilia Udalov <UdalovIlia@gmail.com>
2013-12-14 20:56:38 +0400

    Lab#5 Ilia Udalov. Исправления

--- code/ilia-udalov/test/regex_test.cpp
+++ code/ilia-udalov/test/regex_test.cpp
@@ -7,39 +7,46 @@
 
 #include "library/regex.h"
 
-class RegexTest : public ::testing::Test {
-};
-
 ///////////////////////////////////////////////////////////////////////////////
 // match
 ///////////////////////////////////////////////////////////////////////////////
-TEST_F(RegexTest, Match_Any_String) {
+
+TEST(MatchTest, Match_Basic_Regex) {
     EXPECT_EQ(1, match(std::string(".*"), std::string("banana")));
-}
-
-TEST_F(RegexTest, Match_Same_String) {
     EXPECT_EQ(1, match(std::string("banana"), std::string("banana")));
-}
-
-TEST_F(RegexTest, Not_Match_String) {
     EXPECT_EQ(0, match(std::string("pure functions"),
+        std::string("objects are so awesome")));
+    EXPECT_EQ(1, match(std::string(""),
         std::string("objects are so awesome")));
 }
 
-TEST_F(RegexTest, Match_Beginig_Of_String) {
+TEST(MatchTest, Match_Beginig_Of_String) {
     EXPECT_EQ(1, match(std::string("^is"), std::string("is match")));
-}
-
-TEST_F(RegexTest, Not_Match_Beginig_Of_String) {
     EXPECT_EQ(0, match(std::string("^match"), std::string("is not match")));
 }
 
-TEST_F(RegexTest, Not_Match_End_Of_String) {
+TEST(MatchTest, Match_End_Of_String) {
+    EXPECT_EQ(1, match(std::string("match$"), std::string("is match")));
     EXPECT_EQ(0, match(std::string("not$"), std::string("is not match")));
 }
 
-TEST_F(RegexTest, Match_Regexp_With_Points_String) {
+TEST(MatchTest, Match_Begining_And_End_Of_String) {
+    EXPECT_EQ(1, match(std::string("^repeat$"), std::string("repeat")));
+    EXPECT_EQ(0, match(std::string("^play$"),
+        std::string("I can play guitar!")));
+}
+
+TEST(MatchTest, Match_Regex_With_Points_String) {
     EXPECT_EQ(1, match(std::string("S.W.A.T"), std::string("SOWTAAT?")));
+}
+
+TEST(MatchTest, Match_Complex_Regex) {
+    EXPECT_EQ(1, match(std::string("^My.name.*Ilia$"),
+        std::string("My name is Ilia")));
+    EXPECT_EQ(0, match(std::string("^My.name.*Vova$"),
+        std::string("My name is Ilia")));
+    EXPECT_EQ(1, match(std::string("^My.name.*"),
+        std::string("My name is Jon")));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -47,20 +54,23 @@ TEST_F(RegexTest, Match_Regexp_With_Points_String) {
 ///////////////////////////////////////////////////////////////////////////////
 
 
-TEST_F(RegexTest, Just_Find_String) {
+TEST(FindTest, Just_Find_String) {
     EXPECT_EQ(12, find(std::string("Haskell"),
         std::string("Do you know Haskell well?")));
+    EXPECT_EQ(8, find(std::string("west"),
+        std::string("East or west, home is best.")));
 }
 
-TEST_F(RegexTest, Find_Same_String) {
+TEST(FindTest, Find_Same_String) {
     EXPECT_EQ(0, find(std::string("banana"), std::string("banana")));
 }
 
-TEST_F(RegexTest, Find_Substring_Longer_Then_String) {
+TEST(FindTest, Find_Substring_Longer_Then_String) {
     EXPECT_EQ(-1, find(std::string("banananananana"), std::string("banana")));
+    EXPECT_EQ(-1, find(std::string("yahooooooooooo"), std::string("yahooo")));
 }
 
-TEST_F(RegexTest, Not_Find_String) {
+TEST(FindTest, Not_Find_String) {
     EXPECT_EQ(-1, find(std::string("Copyright"),
         std::string("Copyleft 2013 Ilia Udalov")));
 }
