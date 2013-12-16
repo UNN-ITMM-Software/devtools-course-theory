07f0dbe93a8ec126a98effc6ee39209266c3ae58
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2013-12-16 15:58:20 +0400

    fixed source file and test

--- code/dmitry-vodopyanov/library/src/LengthConvertor.cpp
+++ code/dmitry-vodopyanov/library/src/LengthConvertor.cpp
@@ -9,14 +9,6 @@
 LengthConvertor::LengthConvertor(void) {}
 LengthConvertor::~LengthConvertor(void) {}
 
-void CheckRange(double result);
-void CheckRange(double result) {
-    if (result > DBL_MAX)
-        throw std::string("The result is larger than DBL_MAX");
-    else if (result < -DBL_MAX)
-        throw std::string("The result is less than -DBL_MAX");
-}
-
 Length LengthConvertor::Convert(Length length, LengthUnit UnitOutput) {
     double multiplierMeter[7] = {0.0253999998, 0.3048, 0.914399999,
                                  1609.34401, 1, 1000, 0.01};
@@ -24,7 +16,6 @@ Length LengthConvertor::Convert(Length length, LengthUnit UnitOutput) {
         length.value = multiplierMeter[length.UnitInput]/
                        multiplierMeter[UnitOutput]*length.value;
         length.UnitInput = UnitOutput;
-        CheckRange(length.value);
     } else {
         length.value = -1;
     }
--- code/dmitry-vodopyanov/test/length_convertor_test.cpp
+++ code/dmitry-vodopyanov/test/length_convertor_test.cpp
@@ -18,10 +18,12 @@ TEST_F(LengthConvertorTest, Can_Convert_KMeters_to_Meters) {
     EXPECT_EQ(1300.000000, convertor.Convert({1.3, KMeter}, Meter).value);
 }
 
-TEST_F(LengthConvertorTest,
-       Do_Throw_Exception_When_Value_Is_Larger_Than_DBL_MAX) {
-    EXPECT_THROW(convertor.Convert({DBL_MAX+9999999999, KMeter}, Meter),
-                 std::string);
+TEST_F(LengthConvertorTest, Can_Convert_CMeters_to_KMeters) {
+    EXPECT_EQ(8.000000, convertor.Convert({800000, CMeter}, KMeter).value);
+}
+
+TEST_F(LengthConvertorTest, Can_Convert_Miles_to_Yards) {
+    EXPECT_EQ(14080.000000, convertor.Convert({8, Mile}, Yard).value);
 }
 
 TEST_F(LengthConvertorTest,
