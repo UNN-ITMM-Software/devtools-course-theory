2d534418527b9e5618d9f29e1115be16edc996d8
andrey-golodyaev <andrey_gol28@mail.ru>
2013-12-16 13:09:32 +0400

    Исправил

--- code/andrey-golodyaev/test/complex_calculator_test.cpp
+++ code/andrey-golodyaev/test/complex_calculator_test.cpp
@@ -5,41 +5,41 @@
 
 #include "library/complexcalc.h"
 
-TEST(AppTest, calc_can_Add_Numbers) {
+TEST(ComplexTest, calc_can_Add_Numbers) {
     ComplexNumber a(1, 2);
     ComplexNumber b(2, -3);
     ComplexNumber res(3, -1);
     EXPECT_EQ(res, a + b);
 }
 
-TEST(AppTest, calc_can_Sub_Numbers) {
+TEST(ComplexTest, calc_can_Sub_Numbers) {
     ComplexNumber a(0, 1);
     ComplexNumber b(1, -1);
     ComplexNumber res(-1, 2);
     EXPECT_EQ(res, a - b);
 }
 
-TEST(AppTest, calc_can_Mul_Numbers) {
+TEST(ComplexTest, calc_can_Mul_Numbers) {
     ComplexNumber a(1, 1);
     ComplexNumber b(1, 1);
     ComplexNumber res(0, 2);
     EXPECT_EQ(res, a * b);
 }
 
-TEST(AppTest, calc_can_Div_Numbers) {
+TEST(ComplexTest, calc_can_Div_Numbers) {
     ComplexNumber a(1, 1);
     ComplexNumber b(0, 1);
     ComplexNumber res(1, -1);
     EXPECT_EQ(res, a / b);
 }
 
-TEST(AppTest, calc_can_not_Div_by_Zero) {
+TEST(ComplexTest, calc_can_not_Div_by_Zero) {
     ComplexNumber a(1, 1);
     ComplexNumber b(0, 0);
     EXPECT_THROW(a / b, std::string);
 }
 
-TEST(AppTest, calc_can_Parse_Complex_Number) {
+TEST(ComplexTest, calc_can_Parse_Complex_Number) {
     ComplexNumber a;
     char str[35]="1+i";
     a.Input(str);
