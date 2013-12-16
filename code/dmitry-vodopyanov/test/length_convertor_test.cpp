9426e6cf1d2c4a1d8115de0717c5105d190b9ce7
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2013-12-16 16:13:50 +0400

    fixed a bug again

--- code/dmitry-vodopyanov/test/length_convertor_test.cpp
+++ code/dmitry-vodopyanov/test/length_convertor_test.cpp
@@ -22,9 +22,6 @@ TEST_F(LengthConvertorTest, Can_Convert_CMeters_to_KMeters) {
     EXPECT_EQ(8.000000, convertor.Convert({800000, CMeter}, KMeter).value);
 }
 
-TEST_F(LengthConvertorTest, Can_Convert_Miles_to_Yards) {
-    EXPECT_EQ(14080\.000000, convertor.Convert({8, Mile}, Yard).value);
-}
 
 TEST_F(LengthConvertorTest,
        Do_Throw_Exception_When_Value_Is_Less_Than_Minus_DBL_MAX) {
