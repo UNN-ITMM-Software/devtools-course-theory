e26ab67583b82ca46f3402482b093ddd6f0565e8
AlexandrSmirnov <admin@apelsin-pc.ru>
2013-12-20 04:09:04 -0800

    A little idea, how to delete "delete []"

--- code/alexandr-smirnov/library/src/sort_application.cpp
+++ code/alexandr-smirnov/library/src/sort_application.cpp
@@ -83,7 +83,7 @@ std::string SorterApplication::operator()(int argc, const char** argv) {
         return message_;
 
     int N = atoi(argv[2]);
-    std::unique_ptr <int[]> a (new int[N]);
+    std::unique_ptr <int[]> a(new int[N]);
 
     for (int i = 0; i < N; i++)
       a[i] = atoi(argv[i + 3]);
