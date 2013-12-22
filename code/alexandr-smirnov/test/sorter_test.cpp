b7194e4e12478bf3528b8cca48718ed6ea3df4d7
Alex <admin@apelsin-pc.ru>
2013-12-22 09:22:40 +0400

    Added unit tests

--- code/alexandr-smirnov/test/sorter_test.cpp
+++ code/alexandr-smirnov/test/sorter_test.cpp
@@ -29,3 +29,43 @@ TEST_F(SorterTest, Can_Sort_Positive_Numbers_By_MergeSort) {
     delete [] b;
     delete [] a;
 }
+
+TEST_F(SorterTest, Can_Sort_Numbers_By_HeapSort) {
+    int N = 4;
+    int *a = new int[N];
+    a[0] = 20;
+    a[1] = -15;
+    a[2] = 43;
+    a[3] = -2;
+    int *b = new int[N];
+    b[0] = -15;
+    b[1] = -2;
+    b[2] = 20;
+    b[3] = 43;
+    sorter.Sort(a, N, HEAPSORT);
+    for (int i = 0; i < N; i++) {
+    EXPECT_EQ(a[i], b[i]);
+    }
+    delete [] b;
+    delete [] a;
+}
+
+TEST_F(SorterTest, Can_Sort_Large_Numbers_By_QuickSort) {
+    int N = 4;
+    int *a = new int[N];
+    a[0] = 20000000;
+    a[1] = -15000000;
+    a[2] = 43000000;
+    a[3] = -2000000;
+    int *b = new int[N];
+    b[0] = -15000000;
+    b[1] = -2000000;
+    b[2] = 20000000;
+    b[3] = 43000000;
+    sorter.Sort(a, N, QUICKSORT);
+    for (int i = 0; i < N; i++) {
+    EXPECT_EQ(a[i], b[i]);
+    }
+    delete [] b;
+    delete [] a;
+}
