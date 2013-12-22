f40f3cfb74104b2e90cff43ff2f10c6b055e19c0
Alex <admin@apelsin-pc.ru>
2013-12-22 14:14:42 +0400

    Modified tests

--- code/alexandr-smirnov/test/sorter_test.cpp
+++ code/alexandr-smirnov/test/sorter_test.cpp
@@ -4,6 +4,7 @@
 
 #include <limits.h>
 #include <string>
+#include <vector>
 
 #include "library/sort.h"
 
@@ -13,59 +14,56 @@ class SorterTest : public ::testing::Test {
 };
 
 TEST_F(SorterTest, Can_Sort_Positive_Numbers_By_MergeSort) {
-    int N = 3;
-    int *a = new int[N];
+    unsigned int N = 3;
+    std::vector <int> a(N);
+    unsigned int i;
     a[0] = 2;
     a[1] = 1;
     a[2] = 4;
-    int *b = new int[N];
+    std::vector <int> b(N);
     b[0] = 1;
     b[1] = 2;
     b[2] = 4;
-    sorter.Sort(a, N, MERGESORT);
+    sorter.Sort(&a[0], static_cast<int>(N), MERGESORT);
     for (int i = 0; i < N; i++) {
     EXPECT_EQ(a[i], b[i]);
     }
-    delete [] b;
-    delete [] a;
 }
 
 TEST_F(SorterTest, Can_Sort_Numbers_By_HeapSort) {
-    int N = 4;
-    int *a = new int[N];
+    unsigned int N = 4;
+    std::vector <int> a(N);
+    unsigned int i;
     a[0] = 20;
     a[1] = -15;
     a[2] = 43;
     a[3] = -2;
-    int *b = new int[N];
+    std::vector <int> b(N);
     b[0] = -15;
     b[1] = -2;
     b[2] = 20;
     b[3] = 43;
-    sorter.Sort(a, N, HEAPSORT);
+    sorter.Sort(&a[0], static_cast<int>(N), HEAPSORT);
     for (int i = 0; i < N; i++) {
     EXPECT_EQ(a[i], b[i]);
     }
-    delete [] b;
-    delete [] a;
 }
 
 TEST_F(SorterTest, Can_Sort_Large_Numbers_By_QuickSort) {
-    int N = 4;
-    int *a = new int[N];
+    unsigned int N = 4;
+    std::vector <int> a(N);
+    unsigned int i;
     a[0] = 20000000;
     a[1] = -15000000;
     a[2] = 43000000;
     a[3] = -2000000;
-    int *b = new int[N];
+    std::vector <int> b(N);
     b[0] = -15000000;
     b[1] = -2000000;
     b[2] = 20000000;
     b[3] = 43000000;
-    sorter.Sort(a, N, QUICKSORT);
+    sorter.Sort(&a[0], static_cast<int>(N), QUICKSORT);
     for (int i = 0; i < N; i++) {
     EXPECT_EQ(a[i], b[i]);
     }
-    delete [] b;
-    delete [] a;
 }
