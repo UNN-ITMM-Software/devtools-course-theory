e33b243f58e86d2afe6a24a986efc7b9893d3739
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2014-09-08 10:24:10 +0400

    Add scripts from agile course

--- slides/gen_slides.sh
+++ slides/gen_slides.sh
@@ -1,35 +1,41 @@
-# TODO
-# Add --force option for rebuild
+set -e
 
-rootDir=`git rev-parse --show-cdup`
-if [ "$rootDir" == "" ]; then
-    rootDir="./"
-fi
+echo "Start rendering HTML slides from Markdown"
 
-rawDir="$rootDir/materials/lectures-raw"
-cd $rawDir
+# Get the Git root
+root="$(git rev-parse --show-toplevel)"
+cd $root/slides
 
-outDir="../../../../devtools-course-html"
+# Create directory for rendered slides
+rendered_slides_dir="$root/slides-rendered"
+mkdir -p $rendered_slides_dir
+echo "Created directory: $rendered_slides_dir"
 
 # Generating HTML
+echo -e "Processing folders:\n"
 for dir in $(ls -d [0-9]*/);
 do
     echo "dir = $dir"
     cd $dir
-    for file in ./*.md
-    do
-	    echo "Processing $file"
-	    filename=$(basename "$file")
-	    filename="${filename%.*}"
-	    filename="$outDir/$filename.html"
 
-	    if [ $file -nt $filename ]; then
-	        echo "Writing to $filename"
-	        pandoc -t slidy --self-contained -c ../style/slidy.css $file -o $filename
-	    else
-	        echo "Nothing new to generate..."
-	    fi
-	done
-	echo "---"
-	cd ..
+    f=$(find ./ -name '*.md')
+    if [[ -z $f ]]
+    then
+        echo "ERROR: No *.md file"
+        # exit 1
+    else
+        echo "Processing $f"
+        filename=$(basename "$f")
+        filename="${filename%.*}"
+        rendered="$rendered_slides_dir/$filename.html"
+        if [ $f -nt $rendered ]; then
+            echo "Writing to $rendered"
+            pandoc -t slidy -c ../style/slidy.css --self-contained -o $rendered $f
+        else
+            echo "Nothing new to generate..."
+        fi
+    fi
+
+    echo
+    cd ..
 done
